8aa19092ce440fc05780eccedb0477daafc04aa1
ozancinar <ozancinar86@yahoo.com>
2017-03-09 16:19:18 +0100

    initial commit.

--- R/validManipulate.R
+++ R/validManipulate.R
@@ -12,6 +12,5 @@
 
 validManipulate <- function(low, up) {
    if(low < 2) {stop("the smallest number of clusters can be 2.")}
-   source("validPlot.r")
    manipulate(validPlot(low = low, up = up), low = slider(low, up, step = 1, initial = low), up = slider(low, up, step = 1, initial = up))
 }
